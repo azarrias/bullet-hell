0b212d69f93501ca65396f6428eabae82965c918
azarrias <adolfo.zarrias@gmail.com>
2021-04-16 23:51:38 +0200

    Add box collider to floor and gravity to the player. Check if the player is grounded.

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -6,13 +6,29 @@ public class PlayerMovement : MonoBehaviour
 {
     public CharacterController characterController;
     public float speed = 30f;
+    public float gravity = -9.81f;
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+
+    private Vector2 velocity;
+    private bool isGrounded;
 
     // Update is called once per frame
     void Update()
     {
-        float x = Input.GetAxis("Horizontal");
+        velocity.x = Input.GetAxis("Horizontal") * speed;
+        characterController.Move(Vector2.right * velocity.x * Time.deltaTime);
 
-        Vector3 movement = transform.right * x;
-        characterController.Move(movement * speed * Time.deltaTime);
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundDistance, groundMask);
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+        else
+        {
+            velocity.y += gravity * Time.deltaTime;
+            characterController.Move(Vector2.up * velocity.y * Time.deltaTime);
+        }
     }
 }
