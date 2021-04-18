5535e292ffb4a4c00b03cfc6e7315a43a7d39c7b
azarrias <adolfo.zarrias@gmail.com>
2021-04-18 18:18:28 +0200

    Restore the player's Y position if it goes too deep into colliders due to gravity

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -19,12 +19,22 @@ public class PlayerMovement : MonoBehaviour
     void Update()
     {
         velocity.x = Input.GetAxis("Horizontal") * speed;
-        Debug.Log(velocity.x);
         characterController.Move(Vector2.right * velocity.x * Time.deltaTime);
 
-        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundDistance, groundMask);
+        var collider = Physics2D.OverlapCircle(groundCheck.position, groundDistance, groundMask);
+        isGrounded = collider != null;
         if (isGrounded && velocity.y < 0)
         {
+            // If the ground check went in too deep on the collider, restore the player position
+            var contactY = collider.bounds.center.y + collider.bounds.extents.y;
+            var overlapY = contactY - groundCheck.position.y;
+            if (overlapY > 0.05)
+            {
+                var position = gameObject.transform.position;
+                position.y += overlapY;
+                gameObject.transform.position = position;
+            }
+
             velocity.y = -2f;
             if (Input.GetButtonDown("Jump"))
             {
