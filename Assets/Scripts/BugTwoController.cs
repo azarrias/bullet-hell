a4a4158fdb8c46e6b5f1530af9558e4a5e486f27
azarrias <adolfo.zarrias@gmail.com>
2021-04-21 23:03:24 +0200

    Add Enemies animations and a lot more xD

---
+++ Assets/Scripts/BugTwoController.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BugTwoController : MonoBehaviour
+{
+    public float frequency = 20.0f;
+    public float magnitude = 0.5f;
+    public Vector3 targetPosition;
+    public float movingSpeed = 5f;
+    private Vector3 axis;
+
+    private void Start()
+    {
+        axis = transform.up;
+    }
+
+    void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position + axis * Mathf.Sin(Time.time * frequency) * magnitude, targetPosition, movingSpeed * Time.deltaTime);
+    }
+}
